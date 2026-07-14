e9470d68debd56ea2b4ad7bc8477cdf4320c7f76
kumarharkesh7056-blip <kumarharkesh7056@gmail.com>
2026-07-14 19:01:01 +0530

    Script.Js
    Added Firebase initialization and configuration.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("Welcome to PW Running Time");
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA7QiDQJzVb0V8e20NakVBPzbXiGjjnqzw",
+    authDomain: "pw-running-time-5deb3.firebaseapp.com",
+    projectId: "pw-running-time-5deb3",
+    storageBucket: "pw-running-time-5deb3.firebasestorage.app",
+    messagingSenderId: "4542815071",
+    appId: "1:4542815071:web:49079750ef4354954074ea",
+    measurementId: "G-2RMSF18LPT"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
